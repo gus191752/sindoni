a98bd47507b4faf91e175c70a6c2bbddcd48e9ed
gustavo adolfo mujica perez <122643433+gus191752@users.noreply.github.com>
2024-10-31 22:47:15 -0400

    Update tarjeta1.py

--- tarjeta1.py
+++ tarjeta1.py
@@ -7,11 +7,10 @@ import esp
 import _thread
 
 global continuar
-global rpm
-global prod
-global prod_minuto
-global paso
-global conteo
+global actual
+global anterior
+global cont
+global c1
 
 conexion = network.WLAN(network.STA_IF)  # crea objeto de la conexion wifi
 gc.enable()  # habilita el recolector de basura, limpia la memoria ram del esp32
@@ -26,12 +25,12 @@ led = machine.Pin(2, machine.Pin.OUT)
 # entrada de señales por PULL-UP
 boton_bt1 = machine.Pin(22, machine.Pin.IN, machine.Pin.PULL_UP)
 # entrada de señales por PULL-UP
-# boton_c1 = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_UP)
+boton_c1 = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_UP)
 
 
 def conexion_wifi():  # FUNCION de conexion al wifi
     try:
-        print("estableciendo conexion a la red qq")
+        print("estableciendo conexion a la red ")
         # activa la conexion al wifi
         conexion.active(True)
         lista = conexion.scan()                        # scanea las redes wifi disponibles
@@ -58,7 +57,6 @@ def conexion_wifi():  # FUNCION de conexion al wifi
         print("... reconectando")
         continuar1 = False                               # rompe el bucle while principal
 
-
 def blink():
     led.on()
     # FUNCION que hace titilar el led cuando el algoritmo envia datos
@@ -66,106 +64,32 @@ def blink():
     led.off()
     utime.sleep(1)
 
+###################################  conteo multihilo########################
 
-############################# Interrupcion en el pin 21 ######################################
-def encoder_handler(pin):
-    global paso
-    global conteo
-    paso += 1
-    conteo += 1
-
-############################ Multi_Hilo para contar las RPM ##################################
-
-
-def hilo_conteo():
-    global paso
-   # global rpm
-    global conteo
-    global prod_minuto
-    paso = 0
-    conteo = 0
-    prod_minuto = 0
-
-    encoder = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_UP)
-    encoder.irq(trigger=machine.Pin.IRQ_FALLING, handler=encoder_handler)
-
-    timer_start = utime.ticks_ms()
-
-    while True:
-
-        # usando unicamente retardo
-        #         utime.sleep_ms(1000)
-        #         state= machine.disable_irq()
-        #         rpm=(paso*60)/2
-        #         paso=0
-        #         print(rpm,'RPM')
-        #         machine.enable_irq(state)
-
-        timer_elapsed = utime.ticks_diff(utime.ticks_ms(), timer_start)
-        if timer_elapsed >= 1000:
-            # calculo las rpm
-            state = machine.disable_irq()
-            #rpm = (paso*60)/1024
-            #conteo = paso
-            #paso = 0
-            
-            
-            
-            machine.enable_irq(state)
-            timer_start = utime.ticks_ms()
-           # print(rpm,'RPM')
-            print(conteo, 'pulsos')
-
-
-_thread.start_new_thread(hilo_conteo, ())
-
-############################## Multi_Hilo para contar la produccion ############################
-# def hilo_conteo_produccion():
-#     global paso
-#     global conteo
-#     global prod
-#     global prod_minuto
-#     paso = 0
-#     conteo = 0
-#     prod_minuto = 0
-#
-#     encoder= machine.Pin(23,machine.Pin.IN,machine.Pin.PULL_UP)
-#     #encoder= machine.Pin(23,machine.Pin.IN,machine.Pin.PULL_DOWN)
-#     #encoder.irq(trigger=machine.Pin.IRQ_FALLING, handler=encoder_handler)
-#     encoder.irq(trigger=machine.Pin.IRQ_RISING, handler=encoder_handler)
-#     timer_start= utime.ticks_ms()
-#
-#     while True:
-#
-#         #usando unicamente retardo
-# #         utime.sleep_ms(1000)
-# #         state= machine.disable_irq()
-# #         rpm=(paso*60)/2
-# #         paso=0
-# #         print(rpm,'RPM')
-# #         machine.enable_irq(state)
-#
-#         timer_elapsed = utime.ticks_diff(utime.ticks_ms(),timer_start)
-#         if timer_elapsed >= 1000:  #  ===>>>  cuenta durante 5 minutos
-#             #calculo las rpm
-#             state= machine.disable_irq()
-#
-#             prod = paso
-#             prod_minuto = (paso*60)
-#
-#             paso=0
-#             machine.enable_irq(state)
-#             timer_start= utime.ticks_ms()
-#             #print(prod,'botellas')
-#             #print(prod_minuto,'botellas/minuto')
-#             #print(conteo,'conteo')
-# _thread.start_new_thread(hilo_conteo_produccion,())
-###########################################################################################################
-
+def hilo_conteo():                          #  <<<< bucle while de trabajo >>>>
+    global actual
+    global anterior
+    global cont
+    global c1   
+    actual=0
+    anterior=0
+    cont=0
+    while (True):
+        c1=int(boton_c1.value())          # sensa el estado del pin 21           
+        utime.sleep(0.1)
+        if (c1==0 and anterior==0):
+            actual=1
+        if (c1==1 and anterior==1):
+            anterior=0
+        if (actual==1 and anterior==0):
+            cont = cont + 1
+            print("c1: " + str(cont))
+            anterior=1
+            actual=0
+       
+_thread.start_new_thread(hilo_conteo,())        
 
 ###########################################################################################################
-
-
 global continuar1                                     # se declaran las variables
 global continuar2
 continuar1 = True    # bandera del bucle de conexion al wifi
@@ -193,33 +117,16 @@ while (continuar1):  # <<<< bucle while principal >>>>
         bt1 = int(boton_bt1.value())            # sensa el estado del pin 22
         #c1 = int(boton_c1.value())            # sensa el estado del pin 21
 
-        print("Banda Transportadora 1= "+str(bt1))
-        #print("Contador de Produccion 1= "+str(c1))
+        #print("Banda Transportadora 1= " + str(bt1))
+        #print("Contador de Produccion 1= " + str(cont))
 
         #########################################################################################################################
         gc.collect()                                # limpia la basura de la memoria ram
         # imprime cuanta memoria hay disponible
-        print(gc. mem_free())
+        #print(gc. mem_free())
 
         blink()                                     # hace destellar el led de la tarjeta
 
-########################################################  METODO GET  #########################################
-#         try:
-#             print("realizando peticion GET")
-#             peticion=urequests.get('https://ingenieriamcy.000webhostapp.com/prueba_recibe2.php')  # envia la solicitud a la pagina indicada
-#             utime.sleep(10)
-#             print(peticion.status_code)                                                          # codigo de respuesta de la pagina
-#             print(peticion.text)                                                                 # sin esta linea el codigo da error
-#         except:
-#             print("====>no hay respuesta GET")
-#             contador2=contador2+1
-#             print(str(contador2) + " de intentos de solicitud fallida")
-#             if ((contador2)>4):
-#                 print("...reiniciando conexion")
-#                 conexion.disconnect()
-#                 utime.sleep(10)
-#                 continuar2=False
-#                 contador2=0
 ######################################################### METODO POST   +'$  ='+str( )   #####################################################################
 #        try:
 #            dato = ('&device_label=' + str(dato_tarjeta) +
@@ -229,9 +136,6 @@ while (continuar1):  # <<<< bucle while principal >>>>
 #            print("realizando peticion POST")
 #            # cabezera de la pagina a enviar los datos del formulario                                                                                                                    # tiempo de muestreo
 #            cabezera = {'Content-Type': 'application/x-www-form-urlencoded'}
-#
-#            # envio_datos= urequests.post('https://ingenieriamcy.000webhostapp.com/prueba_recibe2.php',data=datos,headers=cabezera)  # envio de datos metodo post
-#            # envio_datos= urequests.post('https://iotgus.000webhostapp.com//prueba_recibe2.php',data=datos,headers=cabezera)  # envio de datos metodo post
 #            envio_datos = urequests.post(
 #                'https://talleratlas.com/monitoreo_lasamericas/prueba_recibe2.php', data=datos, headers=cabezera)
 #
