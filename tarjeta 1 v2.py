5d3c48e842e504d904d42c8fe279169eab3bff0c
gustavo adolfo mujica perez <122643433+gus191752@users.noreply.github.com>
2024-11-05 21:21:39 -0400

    Update tarjeta 1 v2.py

--- tarjeta 1 v2.py
+++ tarjeta 1 v2.py
@@ -22,57 +22,47 @@ esp.osdebug(None)
 wdt = machine.WDT(timeout=90000)
 # habilita el pin 2 como salida para el led indicador
 led = machine.Pin(2, machine.Pin.OUT)
-
-####################################### entradas#######################################
+####################################### Definicion de entradas #######################################
 # entrada de señales por PULL-UP
 boton_bt1 = machine.Pin(22, machine.Pin.IN, machine.Pin.PULL_UP)  #señal on-off
 # entrada de señales por PULL-UP
 boton_c1 = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_UP)  # señal del contador
 
-
-def conexion_wifi():  # FUNCION de conexion al wifi
+############################## FUNCION de conexion al wifi #########################################
+def conexion_wifi():  
     try:
-        print("estableciendo conexion a la red ")
-        # activa la conexion al wifi
-        conexion.active(True)
+        print("estableciendo conexion a la red ")       
+        conexion.active(True)                          # activa la conexion al wifi
         lista = conexion.scan()                        # scanea las redes wifi disponibles
-#        for red in lista:
-            # crea una lista con las redes disponibles
-            #print(red[0].decode())
-        # se conecta a la red, se debe ingresar aqui usuario y clave de la red conocida
+#        for red in lista:                                                       
+#            print(red[0].decode())                    #  crea una lista con las redes disponibles"
 #        conexion.connect("TP-Link_A3C8", "")
 #        conexion.connect("prueba","12345678")
-        conexion.connect("gus","854317gamp")
-        while not conexion.isconnected():
-            # mientras no haya coneccion imprime un .
-            #print(".")
-            utime.sleep(1)
-        # devuelve los valores de la conexion (direccion ip, mascara, dns, etc)
-        #print(conexion.ifconfig())
-        #print(conexion.config("mac"))
+        conexion.connect("gus","854317gamp")           #  se conecta a la red, se debe ingresar aqui usuario y clave de la red conocida"
+        while not conexion.isconnected():                                                       
+#            print(".")                                # "mientras no haya coneccion imprime un ."
+            utime.sleep(1)                                                      
+#        print(conexion.ifconfig())                    #   devuelve los valores de la conexion (direccion ip, mascara, dns, etc)"
+#        print(conexion.config("mac"))
     except:                                            # si la tarjeta detecta un error
-        #print("error en la conexion")
+#        print("error en la conexion")
         utime.sleep(3)                                 # espera 3 seg
         conexion.disconnect()                          # se desconecta de la red
-        # espera 6 seg para cerrar totalmente
-        utime.sleep(6)
+        utime.sleep(6)                                 # " espera 6 seg para cerrar totalmente"
         print("... reconectando")
         continuar1 = False                               # rompe el bucle while principal
-
-def blink():
+################# FUNCION que hace titilar el led cuando el algoritmo envia datos ###############
+def blink():         
     led.on()
-    # FUNCION que hace titilar el led cuando el algoritmo envia datos
     utime.sleep(0.5)
     led.off()
-    utime.sleep(1)
-    
+    utime.sleep(1)   
     ############################### temporizador ###########################
 def temporizador():
     global cont
     global bandera
     #print("dentro de la funcion temporizador coloca bandera=1")
-    bandera=1
-    
+    bandera=1 
 ############################### POST de datos ###########################
 def post_datos():
     global cont
@@ -116,9 +106,7 @@ def post_datos():
 #                continuar2 = False                               # rompe el bucle de trabajo
 #                # vuelve a cero el contador de envios fallidos
 #                contador = 0
-
 ###################################  conteo multihilo########################
-
 def hilo_conteo():                          #  <<<< bucle while de trabajo >>>>
     global actual
     global anterior
@@ -141,11 +129,10 @@ def hilo_conteo():                          #  <<<< bucle while de trabajo >>>>
             actual=0
             
 _thread.start_new_thread(hilo_conteo,())
-
+########################  configuracion del temporizador #################################
 tim1 = Timer(1)
 tim1.init(mode=Timer.PERIODIC,period=50000,callback=lambda t:temporizador())
-
-###########################################################################################################
+###########################################################################################
 global continuar1                                     # se declaran las variables
 global continuar2
 continuar1 = True    # bandera del bucle de conexion al wifi
@@ -154,35 +141,24 @@ contador = 0         # contador de envios fallidos del metodo post
 contador2 = 0        # contador de recepciones fallidas del metodo get
 contador3 = 0        # contador de envios exitosos del metodo post
 bandera=0
-
-######### tarjeta 1 #######
+######### estado tarjeta 1 #######
 # bt1=0
 # c1=0
-
-## en total 2 variables programadas #######################
+############### en total 2 variables programadas #######################
 
 while (continuar1):  # <<<< bucle while principal >>>>
-    conexion_wifi()                                     # activa la conexion al wifi
-    # si la conexion esta correcta activa la bandera del bucle de trabajo
-    if (conexion.isconnected()):
+    conexion_wifi()                    # activa la conexion al wifi
+    if (conexion.isconnected()):       # si la conexion esta correcta activa la bandera del bucle de trabajo
         continuar2 = True
     while (continuar2):  # <<<< bucle while de trabajo >>>>
-
-        ############################################ captura los datos en la tarjeta##########################################
+        ################################### captura los datos en la tarjeta###############
         dato_tarjeta = 'tarjeta1'
         bt1 = int(boton_bt1.value())            # sensa el estado del pin 22
-        #c1 = int(boton_c1.value())            # sensa el estado del pin 21
-
-        #print("Banda Transportadora 1= " + str(bt1))
-        #print("Contador de Produccion 1= " + str(cont))
-
-        #########################################################################################################################
-        gc.collect()                                # limpia la basura de la memoria ram
-              
-        if (bandera==1):
+        ##################################################################################
+        gc.collect()                                # limpia la basura de la memoria ram              
+        if (bandera==1):  # el temporizador activa la bandera 
             #print("en el bucle if")
-            post_datos()
-            print("ciclo exitoso")
-        
-###########################################################################################################################
-        wdt.feed()                                # WATCHODOG TIMER
+            post_datos()  # envia los datos a la base de datos mysql
+            print("ciclo exitoso")       
+         ######################################### WATCHODOG TIMER########################
+        wdt.feed()                                
