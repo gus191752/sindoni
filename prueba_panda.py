3ff929421ceabf6044c52de45325033dd6b72e1a
gus191752 <gus191752@gmail.com>
2024-12-21 00:17:13 -0400

    pandas

--- prueba_panda.py
+++ prueba_panda.py
@@ -1,10 +1,30 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+
 data= {
    "nombre" : [ "gustavo", "yoselin", "aurora" ],
    "edad": ["41","44","5"]
     
 }
 
-df=pd.DataFrame(data)
+#df=pd.DataFrame(data)
 
-print(df)
+#print(df)
+
+datos = pd.read_csv('C:/Users/gmujica/Desktop/bd.csv', encoding='latin-1', sep='\t')
+
+print(datos)
+#datos.info()
+#datos.describe()
+#datos.head(5)
+x=[2,3,4]
+y=[4,7,15]
+
+plt.plot(x,y)
+plt.xlabel('primera grafica')
+plt.show()
+
+
